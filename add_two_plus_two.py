7546cbc8252577fd092ed99de1b4a7104ef42f45
collinsobasuyi <collins2020job@gmail.com>
2024-06-27 18:12:22 +0100

    Add script to perform addition of 2 plus 2

--- add_two_plus_two.py
+++ add_two_plus_two.py
@@ -1,5 +1,7 @@
-# Add 2 plus 2
+"""
+This module performs a simple addition of 2 plus 2 and prints the result.
+"""
+
 RESULT = 2 + 2
 
-# Print out the result
 print("The result of 2 + 2 is:", RESULT)
